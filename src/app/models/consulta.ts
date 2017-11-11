f86f2f2f846d54717ba807e0374e97ca1eab47bc
Vitor-Lopes <vitorcom3@gmail.com>
2017-11-11 03:45:54 -0200

    Adicionado os dados hardcoded

--- src/app/models/consulta.ts
+++ src/app/models/consulta.ts
@@ -2,8 +2,19 @@ import { Medico } from "app/models/medico";
 import { Exame } from "app/models/exame";
 
 export class Consulta {
-    public data: string = "";
-    public medico: Medico;
-    public exames: Exame[] = [];
-    public descricao: string = "";
+    private _id: number;
+    private _data: string = "";
+    private _hora: string = "";
+    private _medico: Medico;
+    private _exames: Exame[] = [];
+    private _descricao: string = "";
+
+    constructor(id: number, data: string, hora: string, medico: Medico, exames: Exame[], descricao: string) {
+        this._id = id;
+        this._data = data;
+        this._hora = hora;
+        this._medico = medico;
+        this._exames = exames;
+        this._descricao = descricao;
+    }
 }
--- src/app/models/paciente.ts
+++ src/app/models/paciente.ts
@@ -1,8 +1,17 @@
 import { Consulta } from "app/models/consulta";
 
 export class Paciente {
-    public nome: string = "";
-    public cadastroSUS: string = "";
-    public cpf: string = "";
-    public consultas: Consulta[] = [];
+    private _id: number;
+    private _nome: string = "";
+    private _cadastroSUS: string = "";
+    private _cpf: string = "";
+    private _consultas: Consulta[] = [];
+
+    constructor (id: number, nome: string, cadastroSUS: string, cpf: string, consultas: Consulta[]) {
+        this._id = id;
+        this._nome = nome;
+        this._cadastroSUS = cadastroSUS;
+        this._cpf = cpf;
+        this._consultas = consultas;
+    }
 }
